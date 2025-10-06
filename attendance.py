1587a4f11ab2e9c66fd0738f0d9f720822dbf67e
manjunathmane <manjunathmane38@gmail.com>
2025-10-06 20:21:10 +0530

    Create attendance.py

---
+++ attendance.py
@@ -0,0 +1,10 @@
+total_classes = int(input("Enter the total number of classes held: "))
+attended_classes = int(input("Enter the number of classes attended: "))
+atnd_pcent = (attended_classes / total_classes) * 100
+if total_classes > 0 and attended_classes > 0:
+    
+    print("Classes Held:", total_classes)
+    print("Classes Attended:", attended_classes)
+    print("Attendance:",  atnd_pcent,"%")
+else:
+    print("Total classes cannot be zero.")
